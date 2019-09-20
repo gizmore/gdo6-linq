091928d5c3ca9a8e619a81cac74cd7c38db8e4f6
gizmore <gizmore@wechall.net>
2019-09-20 22:44:54 +0200

    6.10-r102_sync

--- Method/EditListLinks.php
+++ Method/EditListLinks.php
@@ -2,13 +2,24 @@
 namespace GDO\Linq\Method;
 
 use GDO\Core\Method;
+use GDO\Form\GDT_Form;
 use GDO\Form\MethodForm;
 
 final class EditListLinks extends MethodForm
 {
+	public function gdoParameters()
+	{
+		
+	}
+	
 	public function execute()
 	{
 	}
+	public function createForm(GDT_Form $form)
+	{
+		
+	}
+
 
 	
 }
